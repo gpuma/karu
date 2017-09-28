1e140848d1a793a099fdb28e4b38c506514dd3c4
Gustavo Puma <vash47@gmail.com>
2017-09-27 19:35:18 -0500

    turned into a python class

--- karu-server/prueba.py
+++ karu-server/prueba.py
@@ -1,32 +1,65 @@
 #import pyautogui
 import socket
+import logging
 #import sys
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_address=('localhost',10000)
+import threading
+import time
 
-print  'starting up on %s port %s' % server_address
-sock.bind(server_address)
+class MySocket(object):
+    def __init__(self, host, port):
+        #logging
+        logging.basicConfig(level=logging.INFO)
+        self.logger=logging.getLogger()
 
-sock.listen(1)
-try:
-    while True:
-        print'waiting for a connection'
-        connection, client_address= sock.accept()
+        self.logger.info("Initializing socket")
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server_address = (host, port)
+        self.max_connections = 5
+        #this is used in Loops to indicate
+        #if the program should stop or keep listening
+        self.listen=True
+
+    def start_listening(self):
+        self.logger.info("Starting up on %s port %s" % self.server_address)
+        self.sock.bind(self.server_address)
+        self.sock.listen(self.max_connections)
+
+        #main loop
+        while self.listen:
+            self.logger.info('waiting for a connection')
+            clientsocket, address= self.sock.accept()
+            self.logger.info( 'connection from', address)
+            self.process_connection(clientsocket, address)
+            self.logger.info('esto no aparecera si es bloqueante')
+
+        self.logger.info('listening stopped!')
+
+    def process_connection(self, connection):
         try:
-            print 'connection from', client_address
-
-            while True:
+            #todo: not sure about this shit!
+            while self.listen:
                 data = connection.recv(16)
-                print 'received "%s"' % data
+                self.logger.info( 'received "%s"' % data)
                 if data:
-                    print 'dending data back to the client'
+                    self.logger.info('sending data back to the client')
                     connection.sendall(data)
                 else:
-                    print 'no mor data from', client_address
+                    self.logger.info( 'no more data from', client_address)
                     break
+        except KeyboardInterrupt:
+            self.logger.info('Interrupted!')
         finally:
             connection.close()
-except KeyboardInterrupt:
-    print 'interrumpido'
-    pass
+
+    def stop_listening(self):
+        self.logger.info('stopping listening on connections...')
+        self.listen=False
+
+
+if __name__ == '__main__':
+    s=MySocket('localhost',10000)
+    t=threading.Thread(target=s.start_listening)
+    t.start()
+    time.sleep(5)
+    s.stop_listening()
